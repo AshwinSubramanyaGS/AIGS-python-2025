7c8998f23d7f51954c9ad5315575af9c1dd00a12
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2025-08-28 15:01:19 +0530

    First Organization

---
+++ A-Section/hello.py
@@ -0,0 +1,10 @@
+
+#creating a variables name, ticket no and taking inputs
+
+name=input("Enter your name:")
+ticket_no=int(input("Enter your ticket no:"))
+
+
+#printing the name
+
+print("welcome Mr/Ms: ",name,"\nTicket No: ",ticket_no)
