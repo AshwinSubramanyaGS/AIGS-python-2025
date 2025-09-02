e35fc623e31bc0e1f72ad25b4cd4c228ebb54f1a
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2025-09-02 12:03:37 +0530

    Day 5

---
+++ A-Section/Lab2-A.py
@@ -0,0 +1,32 @@
+'''
+if else syntax
+
+if condi:
+    indented statements
+    asdas
+    asdasd
+    asdasd
+elif condi :
+    indented statements
+    asdasd
+    asdadsads
+else:
+    indented statements
+    asdasd
+    asdadsads
+
+
+'''
+
+#Even or odd
+#creating variable and taking i/p
+
+number= int(input('enter a no: '))
+string=f"ashwin"
+
+#printing if even or odd
+
+if number % 2 == 0:
+    print(f"The no {number} is Even")
+else:
+    print(f"The no {number} is Odd")
