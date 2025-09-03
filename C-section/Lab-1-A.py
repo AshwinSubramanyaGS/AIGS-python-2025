c45ec19a83295c37b7a8339ff2e5d71e7f56de63
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2025-09-03 16:31:58 +0530

    DaY 6 END

---
+++ C-section/Lab-1-A.py
@@ -0,0 +1,20 @@
+#Write a Python program to declare variables, perform arithmetic operations, and display results.
+
+#Creating vars and taking input
+var1=int(input("Enter a value for 1st var: "))
+var2=int(input("Enter a value for 2nd var: "))
+
+#Perform arth operations and print reslult +,-,*,/,%,//,**
+print(f"The sum of {var1} and {var2} is {var1+var2}")
+
+print(f"The dif of {var1} and {var2} is {var1-var2}")
+
+print(f"The prod of {var1} and {var2} is {var1*var2}")
+
+print(f"The quo of {var1} and {var2} is {var1/var2}")
+
+print(f"The remi of {var1} and {var2} is {var1%var2}")
+
+print(f"The int quo of {var1} and {var2} is {var1//var2}")
+
+print(f"The power of {var1} and {var2} is {var1**var2}")
---
+++ C-section/Lab-2-A.py
@@ -0,0 +1,33 @@
+
+'''
+if condi :
+    indented codes
+    n
+    d
+    e
+    nted codes ind
+
+}elif condi 2:
+    indented codes
+    n
+    d
+    e
+    nted codes ind
+else:
+    indented codes
+    n
+    d
+    e
+    nted codes ind
+'''
+
+a= int(input("Enter the number:"))
+chkEven= a%2
+'''
+if(chkEven==0):
+    print("The number is even")
+else:
+    print("The number is odd")
+'''
+print("The number is ","odd"if(chkEven!=0) else "even")
+
