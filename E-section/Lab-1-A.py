1bc8ab863f3c02d45fb258e6717a1ba9d6c0dd77
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2025-09-03 10:13:21 +0530

    Day 6

---
+++ E-section/Lab-1-A.py
@@ -0,0 +1,16 @@
+#Write a Python program to declare variables, perform arithmetic operations, anddisplay results
+
+#Declaring 2 variables and taking input
+
+varible1=int(input("Enter varible 1: "))
+varible2=int(input("Enter varible 2: "))
+
+#perform operation +, -, *,/,%,//,** and print result
+
+print(f"The sum of {varible1} and {varible2} is {varible1+varible2}")
+print(f"The diff of {varible1} and {varible2} is {varible1-varible2}")
+print(f"The prod of {varible1} and {varible2} is {varible1*varible2}")
+print(f"The quo of {varible1} and {varible2} is {varible1/varible2}")
+print(f"The rem of {varible1} and {varible2} is {varible1%varible2}")
+print(f"The int quo of {varible1} and {varible2} is {varible1//varible2}")
+print(f"The exponent of {varible1} and {varible2} is {varible1**varible2}")
---
+++ E-section/Lab-2-A.py
@@ -0,0 +1,33 @@
+'''
+if condi:
+    indentated code
+    block of code
+elif condi:
+    indentated code
+    block of codes
+
+    
+else:
+    indentated code
+    block of codes
+
+'''
+
+number=int(input("Enter a no: "))
+
+#Print even or odd
+'''
+if number%2==0:
+    print(f"The number {number} is Even")
+else :
+    print(f"The number {number} is Odd")
+'''
+print(f"The number {number} is ","Even" if number%2==0 else "Odd")
+
+
+
+
+
+
+
+
